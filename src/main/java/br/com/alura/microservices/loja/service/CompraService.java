23362c4ea240e3f5abc0e5ea6ab3266171c0116f
JoaoSipauba <joaosipauba@hotmail.com>
2022-07-30 23:33:00 -0300

    fix: alterando Anotation de component para service

--- src/main/java/br/com/alura/microservices/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservices/loja/service/CompraService.java
@@ -4,10 +4,10 @@ import br.com.alura.microservices.loja.controller.dto.CompraDTO;
 import br.com.alura.microservices.loja.controller.dto.InfoFornecedorDTO;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class CompraService {
     public void realizaCompra(CompraDTO compra) {
 
