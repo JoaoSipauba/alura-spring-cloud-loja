ecdd0518087104005b48c8f513d450e759e7894e
JoaoSipauba <joaosipauba@hotmail.com>
2022-08-01 20:06:51 -0300

    feat: chamando fornecedor pelo eureka-service

--- src/main/java/br/com/alura/microservices/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservices/loja/service/CompraService.java
@@ -2,6 +2,7 @@ package br.com.alura.microservices.loja.service;
 
 import br.com.alura.microservices.loja.controller.dto.CompraDTO;
 import br.com.alura.microservices.loja.controller.dto.InfoFornecedorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,11 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
+
+    @Autowired
+    private RestTemplate client;
     public void realizaCompra(CompraDTO compra) {
 
-        RestTemplate client = new RestTemplate();
         ResponseEntity<InfoFornecedorDTO> exchange =
                 client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
                 HttpMethod.GET, null, InfoFornecedorDTO.class);
